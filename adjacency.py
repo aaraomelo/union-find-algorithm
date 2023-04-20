3dfcd30f934f3b3aa26005080c5f7a98e0e0cd1d
Aarão Melo Lopes <aarao.melo.lopes@gmail.com>
2023-04-20 19:46:11 -0400

    feat: Adjacency8 class implementation

--- adjacency.py
+++ adjacency.py
@@ -4,34 +4,58 @@ from abc import ABC, abstractmethod
 
 # ==================== Adajacency Base ======================================
 class Adjacency2D(ABC):
-	def __init__(self):
-		pass 
+    """Base class for 2D adjacencies."""
 
-	@abstractmethod
-	def neighbours(self, point): 
-		pass
+    @abstractmethod
+    def neighbours(self, point):
+        """Return a list of neighbours for the given point."""
+        pass
+
 
 # ===================== 4 Connected Adjacency ===============================
 class Adjacency4(Adjacency2D):
-	def __init__(self):
-		super().__init__()
-		self.offsets = [Point(row=0, col=1), Point(row=1, col=0), 
-					    Point(row=0, col=-1),  Point(row=-1, col=0)]
+    """4-connected adjacency: horizontal and vertical neighbours."""
 
-	def neighbours(self, point):
-		neighbours = []
-		for offset in self.offsets:
-			q = offset + point
-			neighbours.append(offset + point)
-		return neighbours
- 
+    def __init__(self):
+        self.offset_points = [
+            Point(row=0, col=1),
+            Point(row=1, col=0),
+            Point(row=0, col=-1),
+            Point(row=-1, col=0),
+        ]
+
+    def neighbours(self, point):
+        """Return a list of 4-connected neighbours for the given point."""
+        neighbours = []
+        for offset in self.offset_points:
+            neighbour = offset + point
+            neighbours.append(neighbour)
+        return neighbours
+
+
 # ====================== 8 Connected Adjacency ===============================
 class Adjacency8(Adjacency2D):
-	def __init__(self, width, height):
-		super().__init__(width, height)
-		# TODO: 
+    """8-connected adjacency: horizontal, vertical, and diagonal neighbours."""
 
-	def neighbours(self, point):
-		# TODO: 
-		pass
+    def __init__(self, width, height):
+        self.offset_points = [
+            Point(row=-1, col=-1),
+            Point(row=-1, col=0),
+            Point(row=-1, col=1),
+            Point(row=0, col=-1),
+            Point(row=0, col=1),
+            Point(row=1, col=-1),
+            Point(row=1, col=0),
+            Point(row=1, col=1),
+        ]
+        self.width = width
+        self.height = height
 
+    def neighbours(self, point):
+        """Return a list of 8-connected neighbours for the given point."""
+        neighbours = []
+        for offset in self.offset_points:
+            neighbour = offset + point
+            if 0 <= neighbour.row < self.height and 0 <= neighbour.col < self.width:
+                neighbours.append(neighbour)
+        return neighbours
